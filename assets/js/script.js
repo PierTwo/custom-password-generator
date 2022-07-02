414944775249c9b76f27154fa918d4f900112fba
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-07-02 06:59:41 -0500

    Created pop-up boxes that have user choose password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,45 @@
-// Assignment Code
+// Selects id of generate
 var generateBtn = document.querySelector("#generate");
+
+// Generates a password using the user criteria
+function generatePassword() {
+  // Prompts user to enter a vlue for password length
+  passLength = prompt("Please enter a length for the password between 8 and 128.", "E.g. \"16\"");
+
+  // Loops the password length prompt until a proper value is selected
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    passLength = prompt("ERROR: Please enter a value between 8 and 128.", "E.g. \"16\"");
+  };
+
+  // Asks user to confirm if to include different character types
+  passLower = confirm("Do you want to include lowercase characters? \nOK for Yes or Cancel for No.");
+  passUpper = confirm("Do you want to include uppercase characters? \nOK for Yes or Cancel for No.");
+  passNums = confirm("Do you want to include numbers? \nOK for Yes or Cancel for No.");
+  passSpecial = confirm("Do you want to include special characters? \nOK for Yes or Cancel for No.");
+
+  // Creates an array of all character type variables
+  charType = [passLower, passUpper, passNums, passSpecial];
+
+  // checks if all of the variables in the array are false
+  function checkType(charType) {
+    return charType == false;
+  };
+
+  // Loops the character prompts until at least one character type is selected
+  while (charType.every(checkType)) {
+    alert("ERROR: At least one character type needed for password.");
+
+    passLower = confirm("Do you want to include lowercase characters? \nOK for Yes or Cancel for No.");
+
+    passUpper = confirm("Do you want to include uppercase characters? \nOK for Yes or Cancel for No.");
+
+    passNums = confirm("Do you want to include numbers? \nOK for Yes or Cancel for No.");
+
+    passSpecial = confirm("Do you want to include special characters? \nOK for Yes or Cancel for No.");
+
+    charType = [passLower, passUpper, passNums, passSpecial];
+  };
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +48,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
